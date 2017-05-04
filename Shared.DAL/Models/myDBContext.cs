d647d331467ce48df98b6b59c8d48be070115d23
x00142193 <x00142193@LAB216-21>
2017-05-04 11:21:28 +0100

    -Added shared class library -Deleted redundant classes/folders -Deleted Console ClientApp

---
+++ Shared.DAL/Models/myDBContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ead_Mini_project_3.Models
+{
+    public class MyDBContext : DbContext
+    {
+        public MyDBContext() : base("SomeNewDB3")
+        {
+
+        }
+
+        public DbSet<Band> bands { get; set; }
+        public DbSet<Show> shows { get; set; }
+
+    }
+}
