09d8d543200c0650cb32fdcef2566cd304f3e47d
x00142193 <x00142193@LAB216-20>
2017-05-04 14:07:12 +0100

    -Major Refactor. Shared.DAL gone. -Web Api and BandsController added

---
+++ WebService.API/Controllers/BandsController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using ead_Mini_project_3.Models;
+
+namespace WebService.API.Controllers
+{
+    public class BandsController : ApiController
+    {
+        private MyDBContext db = new MyDBContext();
+
+        // GET: api/Bands
+        public IHttpActionResult Getbands()
+        {
+            return Ok(db.bands.OrderBy(s=>s.Genres).ToList());
+        }
+
+        // GET: api/Bands/name
+        [ResponseType(typeof(Band))]
+        public async Task<IHttpActionResult> GetBand(string name)
+        {
+            Band band = await db.bands.FindAsync(name);
+            if (band == null)
+            {
+                return Ok(db.bands.OrderBy(s => s.Genres).ToList());
+            }
+
+            return Ok(band);
+        }
+
+        
+    }
+}
---
+++ ead Mini project 3/Models/myDBContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+
+namespace ead_Mini_project_3.Models
+{
+    public class MyDBContext : DbContext
+    {
+        public MyDBContext() : base("Random")
+        {
+
+        }
+
+        public DbSet<Band> bands { get; set; }
+        public DbSet<Show> shows { get; set; }
+
+    }
+}
