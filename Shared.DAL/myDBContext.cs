26df43ba3364bd03a9476f6f07da766d0e4fa418
x00142193 <x00142193@LAB216-21>
2017-05-04 12:48:23 +0100

    -fixed references for Shared.DAL -deleted models from MVC models folder, they're now in Shared.DAL

--- Shared.DAL/myDBContext.cs
+++ Shared.DAL/myDBContext.cs
@@ -4,11 +4,12 @@ using System.Data.Entity;
 using System.Linq;
 using System.Web;
 
+
 namespace ead_Mini_project_3.Models
 {
     public class MyDBContext : DbContext
     {
-        public MyDBContext() : base("SomeNewDB3")
+        public MyDBContext() : base("Random")
         {
 
         }
